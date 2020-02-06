a2a8d464d03460c9d68dc5c88191a88b3ba14c9f
ProsperChuks <prosperc40@gmail.com>
2020-02-06 15:33:14 +0100

    Writing data to xml

--- writexml.py
+++ writexml.py
@@ -1,1 +1,22 @@
 
+import xml.etree.ElementTree as et
+
+# Instance of ElementTree
+tree = et.ElementTree()
+
+# Root Element Tag
+root = et.Element("root")
+
+# child Element Tag of root
+a = et.Element("a")
+b = et.Element("b")
+# Text to be stored in the a element
+a.text = "five"
+b.text = "Mira"
+
+# Adds the element tag to root
+root.append(a)
+root.append(b)
+
+tree._setroot(root)
+tree.write("write.xml")
